d2b793db7a6c8f36a241e0040ad651cd827aea55
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-08-18 15:42:36 +0530

    home page done

---
+++ Assests/JavaScript/HeaderManagement.js
@@ -0,0 +1,32 @@
+class specialHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header id="nav">
+                <a href="#" class="logo">GenArtLab</a>
+                <ul id="center-nav">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="login.html">Login</a></li>
+                </ul>
+                <div id="menu-icon">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </header>
+        `;
+
+        // JavaScript to set active link
+        const navLinks = this.querySelectorAll('#center-nav a');
+        const currentPath = window.location.pathname.split('/').pop();
+
+        navLinks.forEach(link => {
+            if (link.getAttribute('href') === currentPath) {
+                link.classList.toggle('active');
+            }
+        });
+    }
+}
+
+customElements.define('special-header', specialHeader);
