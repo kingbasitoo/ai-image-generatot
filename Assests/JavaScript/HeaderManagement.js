e800bf3d671a8aafab147c43a0771139d757b5c7
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-08-22 03:16:22 +0530

    Feedback form added

--- Assests/JavaScript/HeaderManagement.js
+++ Assests/JavaScript/HeaderManagement.js
@@ -7,7 +7,7 @@ class specialHeader extends HTMLElement {
                     <li><a href="index.html">Home</a></li>
                     <li><a href="about.html">About</a></li>
                     <li><a href="contact.html">Contact</a></li>
-                    <li><a href="login.html">Login</a></li>
+                    <li><a href="feedback.html">Feedback</a></li>
                 </ul>
                 <div id="menu-icon">
                     <div></div>
