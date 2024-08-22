bcd2a25727a5f1277df24e696e220e9866e5f275
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-08-22 19:13:45 +0530

    some minor changes

--- Assests/JavaScript/HeaderManagement.js
+++ Assests/JavaScript/HeaderManagement.js
@@ -45,20 +45,3 @@ window.onscroll = () => {
     menu.classList.remove('add'); 
     navBar.classList.remove('active');
 }
-
-
-const btn = document.querySelector(".btn");
-const post = document.querySelector(".post");
-const widget = document.querySelector(".star-widget");
-const editBtn = document.querySelector(".edit");
-
-
-btn.onclick = () => {
-    widget.style.display = "none";
-    post.style.display = "block";
-    editBtn.onclick = () =>{
-        post.style.display = "none";
-        widget.style.display = "block";
-    }
-    return false;
-}
