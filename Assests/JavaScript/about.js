0bb748a4f596c1769838237dd582ae738edbc297
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-08-23 07:54:25 +0530

    swiper js implemented

---
+++ Assests/JavaScript/about.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2200,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    breakpoints:{
+        0:{
+            slidesPerView : 1
+        },
+        768:{
+            slidesPerView : 2
+        },
+        1024:{
+            slidesPerView : 3
+        }
+    }
+});
